72b0abd4a2bd3328c69c70ff0b421a13b50d2bd8
eugenehan94 <eugene.han94@gmail.com>
2021-03-20 23:17:27 -0400

    First Commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const gridSquares = document.querySelectorAll(".grid-square");
+const gloves = document.querySelector(".gloves");
+//click on square to block
+
+//computer chooses square to kick into
+console.log(gridSquares);
